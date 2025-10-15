bbec891a7ae2862ac812bf3254b37b151c48c85b
egrevs <egorevseenko203@gmail.com>
2025-10-15 15:09:53 +0200

    Json adapter for localDate

--- src/main/java/itmo/studying/utils/FileManager.java
+++ src/main/java/itmo/studying/utils/FileManager.java
@@ -1,22 +1,30 @@
 package itmo.studying.utils;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 import itmo.studying.data.Worker;
 import itmo.studying.exceptions.InvalidFileException;
 
 import java.io.*;
 import java.lang.reflect.Type;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * Класс для чтения и записи коллекции в файл (JSON) с поддержкой LocalDate.
+ */
 public class FileManager {
     private final String envName;
-    private final Gson gson = new Gson();
+    private final Gson gson;
 
     public FileManager(String envName) {
         this.envName = envName;
+        this.gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+                .setPrettyPrinting()
+                .create();
     }
 
     public HashMap<Integer, Worker> readCollection() {
@@ -36,8 +44,7 @@ public class FileManager {
                 jsonBuilder.append(scanner.nextLine());
             }
             String json = jsonBuilder.toString();
-            Type collectionType = new TypeToken<HashMap<Integer, Worker>>() {
-            }.getType();
+            Type collectionType = new TypeToken<HashMap<Integer, Worker>>() {}.getType();
             return gson.fromJson(json, collectionType);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -62,5 +69,21 @@ public class FileManager {
     @Override
     public String toString() {
         return "FileManager (класс для работы с файлом)";
+    }
+
+    /**
+     * Адаптер для сериализации/десериализации LocalDate.
+     */
+    private static class LocalDateAdapter implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
+        @Override
+        public JsonElement serialize(LocalDate date, java.lang.reflect.Type typeOfSrc, JsonSerializationContext context) {
+            return new JsonPrimitive(date.toString()); // например "2025-10-15"
+        }
+
+        @Override
+        public LocalDate deserialize(JsonElement json, java.lang.reflect.Type typeOfT, JsonDeserializationContext context)
+                throws JsonParseException {
+            return LocalDate.parse(json.getAsString());
+        }
     }
 }
